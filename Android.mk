70d12ffbae8e7a8f90b8d450e792c81d4e78b9d7
D. Andrei Măceș <dmaces@nd.edu>
2017-02-19 15:45:00 -0500

    Disable -Waddress-of-packed-member temporarily
    Change-Id: Ie6b49b27ac8d0d0899eaedec2278feb976d19117

--- Android.mk
+++ Android.mk
@@ -42,6 +42,7 @@ bluetooth_CFLAGS += \
   -DLOG_NDEBUG=1
 
 bluetooth_CONLYFLAGS += -std=c99
+bluetooth_CONLYFLAGS += -Wno-address-of-packed-member
 bluetooth_CPPFLAGS :=
 
 include $(call all-subdir-makefiles)
